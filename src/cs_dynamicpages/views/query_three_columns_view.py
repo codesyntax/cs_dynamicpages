403178cb7065e96c2ba084991c59e159fab4309f
Lur Ibargutxi <libargutxi@codesyntax.com>
2025-07-08 09:34:18 +0200

    three columns query row

--- src/cs_dynamicpages/views/query_three_columns_view.py
+++ src/cs_dynamicpages/views/query_three_columns_view.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 
 # from cs_dynamicpages import _
-from Products.Five.browser import BrowserView
 from zope.interface import implementer
 from zope.interface import Interface
-
+from plone.app.contenttypes.browser.collection import CollectionView
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 class IQueryThreeColumnsView(Interface):
@@ -12,11 +11,12 @@ class IQueryThreeColumnsView(Interface):
 
 
 @implementer(IQueryThreeColumnsView)
-class QueryThreeColumnsView(BrowserView):
+class QueryThreeColumnsView(CollectionView):
     # If you want to define a template here, please remove the template from
     # the configure.zcml registration of this view.
     # template = ViewPageTemplateFile('query_three_columns_view.pt')
 
-    def elements(self):
-        import pdb; pdb.set_trace(); a=1
+    def __call__(self):
+        # Implement your own actions:
+        return self.index()
 
