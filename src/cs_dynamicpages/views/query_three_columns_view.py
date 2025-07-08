60ba11709da76c9c71f9de308d42fb111237ac2d
Lur Ibargutxi <libargutxi@codesyntax.com>
2025-07-08 08:33:05 +0200

    Add view: query-three-columns-view

---
+++ src/cs_dynamicpages/views/query_three_columns_view.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# from cs_dynamicpages import _
+from Products.Five.browser import BrowserView
+from zope.interface import implementer
+from zope.interface import Interface
+
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+class IQueryThreeColumnsView(Interface):
+    """ Marker Interface for IQueryThreeColumnsView"""
+
+
+@implementer(IQueryThreeColumnsView)
+class QueryThreeColumnsView(BrowserView):
+    # If you want to define a template here, please remove the template from
+    # the configure.zcml registration of this view.
+    # template = ViewPageTemplateFile('query_three_columns_view.pt')
+
+    def __call__(self):
+        # Implement your own actions:
+        return self.index()
