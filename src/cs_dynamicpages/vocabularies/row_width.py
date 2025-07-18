d771598fd87352ca1381e64fe81c5193b6661aaa
Lur Ibargutxi <libargutxi@codesyntax.com>
2025-07-18 12:05:39 +0200

    row width options

--- src/cs_dynamicpages/vocabularies/row_width.py
+++ src/cs_dynamicpages/vocabularies/row_width.py
@@ -25,8 +25,9 @@ class RowWidth(object):
         # Just an example list of content for our vocabulary,
         # this can be any static or dynamic data, a catalog result for example.
         items = [
-            VocabItem(u'sony-a7r-iii', _(u'Sony Aplha 7R III')),
-            VocabItem(u'canon-5d-iv', _(u'Canon 5D IV')),
+            VocabItem(u'col-md-6 offset-md-3', _(u'narrow')),
+            VocabItem(u'col-md-8 offset-md-2', _(u'normal')),
+            VocabItem(u'col-md-12', _(u'wide')),
         ]
 
         # Fix context if you are using the vocabulary in DataGridField.
