2fbd9a9e1cbe9824edd915fc1a33664e40880e5c
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-10-08 10:51:55 +0200

    format

--- src/cs_dynamicpages/tests/test_upgrade_step_1004.py
+++ src/cs_dynamicpages/tests/test_upgrade_step_1004.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
-from plone.app.testing import setRoles
-from plone.app.testing import TEST_USER_ID
 # from cs_dynamicpages.testing import CS_DYNAMICPAGES_FUNCTIONAL_TESTING
 from cs_dynamicpages.testing import CS_DYNAMICPAGES_INTEGRATION_TESTING
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
 
 import unittest
 
 
 class UpgradeStepIntegrationTest(unittest.TestCase):
-
     layer = CS_DYNAMICPAGES_INTEGRATION_TESTING
 
     def setUp(self):
-        self.portal = self.layer['portal']
-        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal = self.layer["portal"]
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
 
     def test_upgrade_step(self):
         # dummy, add tests here
