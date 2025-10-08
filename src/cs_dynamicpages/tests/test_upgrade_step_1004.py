908c3f50a28098d238b9702cc441d61e5eb921df
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-10-08 10:56:35 +0200

    lint

--- src/cs_dynamicpages/tests/test_upgrade_step_1004.py
+++ src/cs_dynamicpages/tests/test_upgrade_step_1004.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # from cs_dynamicpages.testing import CS_DYNAMICPAGES_FUNCTIONAL_TESTING
 from cs_dynamicpages.testing import CS_DYNAMICPAGES_INTEGRATION_TESTING
 from plone.app.testing import setRoles
--- src/cs_dynamicpages/upgrades/v1004.py
+++ src/cs_dynamicpages/upgrades/v1004.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 from . import logger
 from .base import reload_gs_profile
