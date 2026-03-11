3f45ce060ed7fef90239909264b7374e5c7187de
Lur Ibargutxi <libargutxi@codesyntax.com>
2026-03-11 15:16:26 +0100

    available templates change

--- src/cs_dynamicpages/views/dynamic_view.py
+++ src/cs_dynamicpages/views/dynamic_view.py
@@ -8,6 +8,9 @@ from Products.Five.browser import BrowserView
 from zope.interface import alsoProvides
 from zope.interface import implementer
 from zope.interface import Interface
+from Acquisition import aq_parent
+from zope.i18n import translate
+from plone.app.uuid.utils import uuidToObject
 
 
 class IDynamicView(Interface):
@@ -76,8 +79,26 @@ class DynamicView(BrowserView):
         )
 
     def available_templates(self):
+        portal_types = api.portal.get_tool("portal_types")
         manager = Manager(self.context)
-        return manager.get_templates()
+        templates = []
+        for template in manager.get_templates():
+            obj = uuidToObject(template.get("uid"))
+            if obj:
+                parent = aq_parent(obj)
+                template.update({
+                    "Title": parent.Title(),
+                    "Description": parent.Description(),
+                    "absolute_url": parent.absolute_url(),
+                    "portal_type": parent.portal_type,
+                    "translated_portal_type": translate(
+                        portal_types.get(parent.portal_type).title,
+                        domain=portal_types.get(parent.portal_type).i18n_domain,
+                        context=self.request,
+                    ),
+                })
+                templates.append(template)
+        return templates
 
     def self_in_templates(self):
         if "rows" in self.context:
