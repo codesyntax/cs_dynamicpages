37ca9cd7dc13de9b80af2c4758c84d924b625ba2
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-06-29 08:14:26 +0200

    upgrade step implementation

--- src/cs_dynamicpages/upgrades/v1010.py
+++ src/cs_dynamicpages/upgrades/v1010.py
@@ -2,11 +2,10 @@ from . import logger
 
 
 from .base import reload_gs_profile
+
 # from plone import api
 
 
 def upgrade(setup_tool=None):
-    """
-    """
+    """ """
     logger.info("Running upgrade (Python): Fix Manage Templates Link")
-    reload_gs_profile(setup_tool)
