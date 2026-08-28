e1098f9486a4ebc0fb3d7b5be9b75d2e1be734c8
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-08-28 11:20:46 +0200

    feat: index implementation

--- src/cs_dynamicpages/indexers/row_type_index.py
+++ src/cs_dynamicpages/indexers/row_type_index.py
@@ -1,15 +1,15 @@
-from plone.app.contenttypes.interfaces import IDocument
+from cs_dynamicpages.content.dynamic_page_row import IDynamicPageRow
 from plone.dexterity.interfaces import IDexterityContent
 from plone.indexer import indexer
 
 
 @indexer(IDexterityContent)
 def dummy(obj):
-    """ Dummy to prevent indexing other objects thru acquisition """
-    raise AttributeError('This field should not indexed here!')
+    """Dummy to prevent indexing other objects thru acquisition"""
+    raise AttributeError("This field should not indexed here!")
 
 
-@indexer(IDocument)  # ADJUST THIS!
+@indexer(IDynamicPageRow)  # ADJUST THIS!
 def row_type_index(obj):
     """Calculate and return the value for the indexer"""
-    return obj.row_type_index
+    return obj.row_type
