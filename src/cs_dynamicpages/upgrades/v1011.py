0102531437dce736aba21a550a824148520fd3fe
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-08-28 12:49:15 +0200

    remove unneeded call

--- src/cs_dynamicpages/upgrades/v1011.py
+++ src/cs_dynamicpages/upgrades/v1011.py
@@ -1,11 +1,9 @@
 from . import logger
 from .base import reload_gs_profile
 
-
 # from plone import api
 
 
 def upgrade(setup_tool=None):
     """ """
     logger.info("Running upgrade (Python): Install new index and reindex")
-    reload_gs_profile(setup_tool)
