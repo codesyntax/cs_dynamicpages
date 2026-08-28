a355da5c35f3c17d7310807a9a27af6a559d9f0e
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-08-28 11:19:33 +0200

    Add indexer: row_type_index

---
+++ src/cs_dynamicpages/indexers/row_type_index.py
@@ -0,0 +1,15 @@
+from plone.app.contenttypes.interfaces import IDocument
+from plone.dexterity.interfaces import IDexterityContent
+from plone.indexer import indexer
+
+
+@indexer(IDexterityContent)
+def dummy(obj):
+    """ Dummy to prevent indexing other objects thru acquisition """
+    raise AttributeError('This field should not indexed here!')
+
+
+@indexer(IDocument)  # ADJUST THIS!
+def row_type_index(obj):
+    """Calculate and return the value for the indexer"""
+    return obj.row_type_index
---
+++ src/cs_dynamicpages/tests/test_indexer_row_type_index.py
@@ -0,0 +1,30 @@
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from cs_dynamicpages.testing import FUNCTIONAL_TESTING
+from cs_dynamicpages.testing import INTEGRATION_TESTING
+
+import unittest
+
+
+class IndexerIntegrationTest(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_dummy(self):
+        self.assertTrue(True)
+
+
+class IndexerFunctionalTest(unittest.TestCase):
+
+    layer = FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+
+    def test_dummy(self):
+        self.assertTrue(True)
