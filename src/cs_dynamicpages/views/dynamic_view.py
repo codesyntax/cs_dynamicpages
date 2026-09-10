441acd14bb2ca40957980b8c0d168015353f9d02
Lur Ibargutxi <libargutxi@codesyntax.com>
2026-09-10 09:31:47 +0200

    return None if no rows folder or is a private state for Anon user

--- src/cs_dynamicpages/views/dynamic_view.py
+++ src/cs_dynamicpages/views/dynamic_view.py
@@ -48,6 +48,7 @@ class DynamicView(BrowserView):
                 )
                 api.content.transition(created_element, transition="publish")
                 return created_element
+        return None
 
     def dynamic_page_folder_element_url(self):
         dynamic_page_folder = self.dynamic_page_folder_element()
@@ -63,8 +64,7 @@ class DynamicView(BrowserView):
 
     def normalize_title(self, title):
         return (
-            title
-            .replace("cs_dynamicpages-", " ")
+            title.replace("cs_dynamicpages-", " ")
             .replace("-", " ")
             .replace("_", " ")
             .replace("view", "")
@@ -79,17 +79,19 @@ class DynamicView(BrowserView):
             obj = uuidToObject(template.get("uid"))
             if obj:
                 parent = aq_parent(obj)
-                template.update({
-                    "Title": parent.Title(),
-                    "Description": parent.Description(),
-                    "absolute_url": parent.absolute_url(),
-                    "portal_type": parent.portal_type,
-                    "translated_portal_type": translate(
-                        portal_types.get(parent.portal_type).title,
-                        domain=portal_types.get(parent.portal_type).i18n_domain,
-                        context=self.request,
-                    ),
-                })
+                template.update(
+                    {
+                        "Title": parent.Title(),
+                        "Description": parent.Description(),
+                        "absolute_url": parent.absolute_url(),
+                        "portal_type": parent.portal_type,
+                        "translated_portal_type": translate(
+                            portal_types.get(parent.portal_type).title,
+                            domain=portal_types.get(parent.portal_type).i18n_domain,
+                            context=self.request,
+                        ),
+                    }
+                )
                 templates.append(template)
         return templates
 
